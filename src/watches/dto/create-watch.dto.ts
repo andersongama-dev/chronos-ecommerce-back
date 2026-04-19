f5d1e7a5340985a83ce6715b861129a9596f14ea
andersongama-dev <andersongamasilva5@gmail.com>
2026-04-19 19:06:30 -0300

    feat(watchs): create CRUD full

--- src/watches/dto/create-watch.dto.ts
+++ src/watches/dto/create-watch.dto.ts
@@ -1,1 +1,44 @@
-export class CreateWatchDto {}
+import {
+    IsString,
+    IsEnum,
+    IsDecimal,
+    IsUrl,
+    IsNotEmpty,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { Brand, Color, Gender, Material, Style } from 'src/generated/prisma/enums';
+
+export class CreateWatchDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsEnum(Brand)
+    brand: Brand;
+
+    @IsDecimal()
+    @Type(() => Number)
+    price: number;
+
+    @IsEnum(Gender)
+    gender: Gender;
+
+    @IsEnum(Style)
+    style: Style;
+
+    @IsEnum(Material)
+    caseMaterial: Material;
+
+    @IsEnum(Material)
+    strapMaterial: Material;
+
+    @IsEnum(Color)
+    strapColor: Color;
+
+    @IsUrl()
+    imageUrl: string;
+
+    @IsString()
+    @IsNotEmpty()
+    slug: string;
+}
